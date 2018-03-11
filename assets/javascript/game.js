87fc5ae37d8c283e9b29e6b5a5fdc4c1eb35d213
Benjamin Shim <benjaminshim@Benjamins-MacBook-Pro.local>
2018-03-10 18:52:53 -0800

    Finish pseudocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,33 @@
+// CREATE ARRAY OF LETTERS OF THE ALPHAPET TO BE GUESSED
+
+// CREATE GLOBAL VARIABLES
+
+// CREATE A VARIABLE THAT STORES AN ARRAY OF ALL OF THE USER'S
+//  GUESSES EACH ROUND
+
+// CREATE A VARIABLE THAT REPRESENTS USER'S KEY PRESS
+
+// CREATE A VARIABLE THAT STORE'S COMPUTER'S RANDOM LETTER
+
+// LISTEN FOR KEY PRESS EVENT
+    
+    // STORE USER'S KEYPRESS INTO userGuess
+   
+    // IF userGuess IS INCORRECT 
+        // STORE USER'S GUESS TO alreadyGuessed ARRAY
+        // SUBTRACT guessesLeft BY 1
+        // *userGuess MUST BE A LETTER, NOT ANY OTHER KEY*
+ 
+    // IF userGuess == computerGuess: 
+        // wins +1
+        // RESET guessesLeft TO 9
+        // RESET ARRAY OF alreadyGuessed TO 0
+        // COMPUTER SHOULD RE-PICK A RANDOM LETTER
+
+
+   
+    // 10 IF guessesLeft REACHES 0:
+        // losses +1
+        // RESET guessesLeft TO 9
+        // RESET ARRAY OF alreadyGuessed TO 0
+        // COMPUTER SHOULD RE-PICK A RANDOM LETTER
